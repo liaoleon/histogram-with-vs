780ab58da246d22356a1f429213b969475f23b97
liaoleon <leon555phs611@gmail.com>
2013-11-17 16:02:53 +0800

    modify histogram.cpp

--- histogram/histogram/histogram.cpp
+++ histogram/histogram/histogram.cpp
@@ -2,45 +2,40 @@
 #include <cv.h>
 #include <highgui.h>
 #include <iostream>
-using namespace std ;
+using namespace std;
+using namespace cv;
 
 int main( int argc, char **argv )
 {
 
 
-	IplImage *img01;
-	CvSize imgsize01 = cvSize(300,300);  // 給圖像SIZE
-	img01 = cvCreateImage(imgsize01,IPL_DEPTH_8U,3);//創造img01
+	char* FileName="DSC_0122.jpg";
+	IplImage *image1 = cvLoadImage(FileName,CV_LOAD_IMAGE_COLOR);
 
-	int Height=cvGetDimSize(img01,0);//讀取矩陣高度
-	int Width=cvGetDimSize(img01,1);//讀取矩陣寬度
+	int Height=cvGetDimSize(image1,0);//讀取矩陣高度
+	int Width=cvGetDimSize(image1,1);//讀取矩陣寬度
+	double B[4000][3000];
+	CvScalar Scalar1;
 
-	//這裡先給img01全白
-	for(int y = 0 ; y < Height ; y++)
-		for(int x = 0 ; x < Width ; x++ )
-			cvSet2D(img01,y,x,CV_RGB(255,255,255));//特別注意 給值是 (y,x)
-
-	for(int y = 0 ;y < Width ; y++)
-		cvSet2D(img01,y,10,CV_RGB(255,0,0));//特別注意 給值是 (y,x)
-
-	for(int x = 0 ;x < Width ; x++)
-		cvSet2D(img01,10,x,CV_RGB(0,255,0));//特別注意 給值是 (y,x)
-
-
-	cout<<"(10,100)R ="<<cvGet2D(img01, 100 , 10 ).val[2]<<endl;//試著讀取某個點的RGB值
-	cout<<"(10,100)G ="<<cvGet2D(img01, 100 , 10 ).val[1]<<endl;//特別注意是cvGet2D(img,y,x)
-	cout<<"(10,100)B ="<<cvGet2D(img01, 100 , 10 ).val[0]<<endl;//所以在輸入XY時 X 和 Y 要對調
-
-
-	cvNamedWindow("img01",CV_WINDOW_AUTOSIZE);
-	cvShowImage("img01", img01);
-	//輸出img01
+	for(int i=1;i<=Height;i++){
+		for (int j=1;j<=Width;j++){
+			 Scalar1=cvGet2D(image1,i,j);
+			 B[i][j]=Scalar.val[0];
+			
+		}
+	}
 
 
 
+	
+    cvNamedWindow("Show Image",0);
+	cvResizeWindow("Show Image",774,510);
+    cvShowImage("Show Image",image1);
 
-	cvWaitKey(0);
-	cvReleaseImage(&img01);
-	cvDestroyAllWindows();
+    cvWaitKey(0);
+
+    cvDestroyWindow("Show Image");
+
+    cvReleaseImage(&image1);
 	return 0;
 }
