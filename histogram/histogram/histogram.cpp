5d4ab7a1f304d8a598c56e58ebf1113973a7f3d4
liaoleon <leon555phs611@gmail.com>
2013-11-17 17:02:51 +0800

    output gray level graphical

--- histogram/histogram/histogram.cpp
+++ histogram/histogram/histogram.cpp
@@ -1,4 +1,4 @@
- #include <stdio.h>
+ï»¿#include <stdio.h>
 #include <cv.h>
 #include <highgui.h>
 #include <iostream>
@@ -11,26 +11,36 @@ int main( int argc, char **argv )
 
 	char* FileName="DSC_0122.jpg";
 	IplImage *image1 = cvLoadImage(FileName,CV_LOAD_IMAGE_COLOR);
-
-	int Height=cvGetDimSize(image1,0);//Åª¨ú¯x°}°ª«×
-	int Width=cvGetDimSize(image1,1);//Åª¨ú¯x°}¼e«×
-	double B[4000][3000];
+	IplImage *image2 = cvCreateImage(cvGetSize(image1),IPL_DEPTH_8U,3);
+ 	int Height=cvGetDimSize(image1,0);//è®€å–çŸ©é™£é«˜åº¦
+	int Width=cvGetDimSize(image1,1);//è®€å–çŸ©é™£å¯¬åº¦
+	double Y,U,V;
 	CvScalar Scalar1;
-
-	for(int i=1;i<=Height;i++){
-		for (int j=1;j<=Width;j++){
+	
+	for(int i=0;i<Height;i++){
+		for (int j=0;j<Width;j++){
 			 Scalar1=cvGet2D(image1,i,j);
-			 B[i][j]=Scalar.val[0];
+			 Y=Scalar1.val[2]*0.299+0.587*Scalar1.val[1]+0.114*Scalar1.val[0];
+			 //U=0.436*(Scalar1.val[0]-Y)/(1-0,114)+128;
+			 //V=0.615*(Scalar1.val[2]-Y)/(1-0.299)+128;
+			 if(Y<64)
+				cvSet2D(image2,i,j,cvScalar(0,0,0));
+			 else
+				cvSet2D(image2,i,j,cvScalar(255,255,255));
+			 
 			
 		}
 	}
-
-
-
+	
 	
     cvNamedWindow("Show Image",0);
 	cvResizeWindow("Show Image",774,510);
     cvShowImage("Show Image",image1);
+
+	cvNamedWindow("Show Image2",0);
+	cvResizeWindow("Show Image2",774,510);
+    cvShowImage("Show Image2",image2);
+
 
     cvWaitKey(0);
 
