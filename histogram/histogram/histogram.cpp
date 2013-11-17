39c7916bbc1394cfebe9632d82c8c074d7db5e10
liaoleon <leon555phs611@gmail.com>
2013-11-17 18:27:51 +0800

    test pdf

--- histogram/histogram/histogram.cpp
+++ histogram/histogram/histogram.cpp
@@ -8,31 +8,67 @@ using namespace cv;
 int main( int argc, char **argv )
 {
 
-
+	int GrayLevel[256]={0};
+	double pdf[256]={0};
+	double cdf[256]={0};
+	uchar outputdata[256]={0};
 	char* FileName="DSC_0122.jpg";
 	IplImage *image1 = cvLoadImage(FileName,CV_LOAD_IMAGE_COLOR);
-	IplImage *image2 = cvCreateImage(cvGetSize(image1),IPL_DEPTH_8U,3);
+	IplImage *image2 = cvLoadImage(FileName,CV_LOAD_IMAGE_COLOR);
+	IplImage *image3 = cvCreateImage(cvGetSize(image2),IPL_DEPTH_8U,3);
  	int Height=cvGetDimSize(image1,0);//讀取矩陣高度
 	int Width=cvGetDimSize(image1,1);//讀取矩陣寬度
-	double Y,U,V;
+	int Y;
+	int temp=0;
+	int temp2=0;
 	CvScalar Scalar1;
 	
 	for(int i=0;i<Height;i++){
 		for (int j=0;j<Width;j++){
 			 Scalar1=cvGet2D(image1,i,j);
-			 Y=Scalar1.val[2]*0.299+0.587*Scalar1.val[1]+0.114*Scalar1.val[0];
+			 Y=(int)Scalar1.val[2]*0.299+0.587*Scalar1.val[1]+0.114*Scalar1.val[0];
 			 //U=0.436*(Scalar1.val[0]-Y)/(1-0,114)+128;
 			 //V=0.615*(Scalar1.val[2]-Y)/(1-0.299)+128;
-			 if(Y<64)
-				cvSet2D(image2,i,j,cvScalar(0,0,0));
-			 else
-				cvSet2D(image2,i,j,cvScalar(255,255,255));
+			GrayLevel[Y]++;		 
 			 
-			
+		}
+	}
+	//pdf
+	for(int i=0;i<256;i++){
+		pdf[i]=(GrayLevel[i]/10036224);
+	}
+
+
+	for(int i=0;i<256;i++){
+		cout<< pdf[i]<<endl;
+		temp++;
+		temp2+=GrayLevel[i];
+	}
+
+	cout<<"temp:"<<temp<<endl;
+	cout<<"GreyLevel:"<<temp2<<endl;
+	cout<<"Height*Width:"<<Height*Width<<endl;
+
+	//cdf
+	for(int i=0;i<256;i++){
+		for (int j=0;j<=i;j++){
+			cdf[i]+=pdf[j];
 		}
 	}
 	
-	
+	for(int i=0;i<256;i++)
+    {
+        outputdata[i]=(uchar)(255*cdf[i]);
+		
+    }
+
+	//for(int i=0;i<Height;i++){
+	//	for (int j=0;j<Width;j++){
+	//		 Scalar1=cvGet2D(image2,i,j);
+	//		 cvSet2D(image3,i,j,cvScalar(0,0,0));
+	//	}
+	//}
+
     cvNamedWindow("Show Image",0);
 	cvResizeWindow("Show Image",774,510);
     cvShowImage("Show Image",image1);
